fcf912afee9d6b4db037989ce9be40ec74e809b1
Tommi Alajoki <tommi@alajoki.fi>
2020-12-14 04:15:31 +0200

    config only now loads dotenv file

--- config/config.js
+++ config/config.js
@@ -1,15 +1,2 @@
-let config = {};
+import "https://deno.land/x/dotenv/load.ts";
 
-if (Deno.env.get('TEST_ENVIRONMENT')) {
-    config.database = {};
-  } else {
-    config.database = {
-      hostname: "lyrete.me",
-      database: "tommidb",
-      user: "tommidb",
-      password: "morso",
-      port: 5432
-    };
-  }
-
-export { config }; 
