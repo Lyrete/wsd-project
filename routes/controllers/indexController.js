4dc6dd434bd6db1b2c1d16d84564c0cf78dfda32
Tommi Alajoki <tommi@alajoki.fi>
2020-12-14 06:23:19 +0200

    fetch the data for the index

--- routes/controllers/indexController.js
+++ routes/controllers/indexController.js
@@ -1,5 +1,17 @@
-const showIndex = async({render}) => {
-    render('index.ejs');
+import { getDay } from "../../services/summaryService.js";
+
+const showIndex = async({render, session}) => {
+    const data = {
+        email: await session.get('userEmail')
+    };
+
+    //get the data for today and yesterday
+    const date = new Date();
+    data.today = await getDay(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
+    date.setDate(date.getDate() - 1);
+    data.yesterday = await getDay(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
+    
+    render('index.ejs', data);
 }
 
 export { showIndex };
